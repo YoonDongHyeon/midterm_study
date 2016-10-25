653c1d5d93be3f5db83334173b3bcedb3deb091c
YoonDongHyeon <dhdh9595@naver.com>
2016-10-25 10:25:31 +0900

    중간 공부

--- midtermstudy.py
+++ midtermstudy.py
@@ -312,5 +312,46 @@
 # print('You are Correct by {0:.2f} times'.format(count))
 # print('average of strike is {0:.2f}'.format(sum_strike/count))
 # print('average of ball is {0:.2f}'.format(sum_ball/count))
-
-
+tree = {
+    'O': {'A': 2, 'C': 4, 'B': 5},
+    'A': {'O': 2, 'B': 2, 'D': 7},
+    'B': {'O': 5, 'A': 2, 'C': 1, 'D':4, 'E':3},
+    'C': {'O': 4, 'B': 1, 'E': 4},
+    'D': {'B': 4, 'E': 1, 'T':5},
+    'E': {'B': 3, 'D': 1, 'T': 7},
+    'T': {'D': 5, 'E': 7}
+}
+[5,3,2,4,7]
+X = []
+for i in tree.keys():
+    for j in tree[i].keys():
+        X.append([tree[i][j],i,j])
+for i in range(0,len(X)-1):
+    for j in range(i,len(X)):
+        if X[i][0]> X[j][0]:
+            t=X[i]
+            X[i]=X[j]
+            X[j]=t
+starting_point=[X[0][1],X[0][2]]
+Y=[]
+ans=[X[0]]
+while True:
+ for i in starting_point:
+    for j in tree[i].keys():
+        if not j in starting_point:
+            Y.append([tree[i][j],i,j])
+ print("후보군:",Y)
+ for i in range(len(Y)-1):
+    for j in range(i,len(Y)):
+        if Y[i][0]>Y[j][0]:
+            t=Y[i]
+            Y[i]=Y[j]
+            Y[j]=t
+ ans.append(Y[0])
+ starting_point.append(Y[0][2])
+ Y=[]
+ print("갱신값",ans)
+ print("완료점",starting_point)
+ print("-"*50)
+ if len(starting_point)==7:
+     break
