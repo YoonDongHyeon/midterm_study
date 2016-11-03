0a1b2a0fb7978bf3b1f7e1715fb2bbb56e4763a7
YoonDongHyeon <dhdh9595@naver.com>
2016-11-03 19:22:23 +0900

    중간 공부2

--- 20161103_2013311994_YoonDongHyeon.py
+++ 20161103_2013311994_YoonDongHyeon.py
@@ -7,7 +7,7 @@ machine_part = [
     [0, 0, 0, 0, 1, 1, 0],
 
 ]
-
+ 
 m = (len(machine_part))
 n = (len(machine_part[0]))
 machine_name = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
